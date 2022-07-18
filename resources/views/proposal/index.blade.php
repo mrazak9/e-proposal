f98f9d956f4e4c8fb2fda1c677150ebc40c03275
codeguna <pratamagunadhi@gmail.com>
2022-07-18 10:52:50 +0700

    improvement UI

--- resources/views/proposal/index.blade.php
+++ resources/views/proposal/index.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-    <div class="container-fluid">
+    {{-- <div class="container-fluid">
         <div class="row">
             <div class="col-sm-12">
                 <div class="card">
@@ -63,7 +63,8 @@
                                                     Ditolak
                                                 @endif
                                             </td>
-                                            <td><span class="alert alert-warning">{{ $proposal->revision->count() }}</span></td>
+                                            <td><span class="alert alert-warning">{{ $proposal->revision->count() }}</span>
+                                            </td>
                                             <td>
                                                 <form action="{{ route('admin.proposals.destroy', $proposal->id) }}"
                                                     method="POST">
@@ -89,5 +90,116 @@
                 {!! $proposals->links() !!}
             </div>
         </div>
+    </div><br /> --}}
+    <div class="card">
+        <div class="card-header">
+            <div style="display: flex; justify-content: space-between; align-items: center;">
+
+                <span id="card_title">
+                   <h4>Daftar Proposal</h4>
+                </span>
+
+                <div class="float-right">
+                    <a href="{{ route('admin.proposals.create') }}" class="btn btn-primary btn-sm float-right"
+                        data-placement="left">
+                        {{ __('Create New') }}
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="card-body"></div>
     </div>
+    <br/>
+        @foreach ($proposals as $proposal)
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-3">
+                        <h5>No. / Nama Proposal</h5>
+                    </div>
+                    <div class="col-md-3">
+                        <h5>Tempat</h5>
+                    </div>
+                    <div class="col-md-3">
+                       <h5>Jenis</h5>
+                    </div>
+                    <div class="col-md-3">
+                       <h5>Tanggal</h5>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3">
+                        {{ ++$i }}. {{ $proposal->name }}
+                    </div>
+                    <div class="col-md-3">
+                        {{ $proposal->place->name }}
+                    </div>
+                    <div class="col-md-3">
+                        {{ $proposal->event->name }}
+                    </div>
+                    <div class="col-md-3">
+                        {{ $proposal->tanggal }}
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-md-3">
+                        <h5>Status</h5>
+                    </div>
+                    <div class="col-md-3">
+                        <h5>Revisi</h5>
+                    </div>
+                    <div class="col-md-3">
+
+                    </div>
+                    <div class="col-md-3">
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3">
+                        @if ($proposal->approval->approved == 1)
+                            <span class="badge bg-success" style="color: white">Disetujui</span>
+                        @else
+                           <span class="badge bg-warning">Proses Pengajuan</span> 
+                        @endif
+                    </div>
+                    <div class="col-md-3">
+                        <span class="badge bg-warning" style="color: white">{{ $proposal->revision->count() }}</span>
+                    </div>
+                    <div class="col-md-3">
+
+                    </div>
+                    <div class="col-md-3">
+
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-xs-4">
+                       <h5>Aksi</h5>
+                    </div>
+                    <form action="{{ route('admin.proposals.destroy', $proposal->id) }}"
+                            method="POST">
+                    <div class="col-xs-8">
+                        
+                            <a class="btn btn-sm btn-primary "
+                                href="{{ route('admin.proposals.show', $proposal->id) }}"><i
+                                    class="fa fa-fw fa-eye"></i> Show</a>
+                            <a class="btn btn-sm btn-success"
+                                href="{{ route('admin.proposals.edit', $proposal->id) }}"><i
+                                    class="fa fa-fw fa-edit"></i> Edit</a>
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger btn-sm"><i
+                                    class="fa fa-fw fa-trash"></i> Delete</button>
+                        
+                    </div>
+                </form>
+                </div>
+            </div>
+    </div> 
+    <br/>
+    @endforeach
+    {!! $proposals->links() !!}
 @endsection
