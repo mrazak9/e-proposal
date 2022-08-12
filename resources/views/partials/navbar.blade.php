2e1abfc9a97cb569f2775a1ecd037b66ce3ebcd9
codeguna <pratamagunadhi@gmail.com>
2022-08-12 13:29:06 +0700

    create notification

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -24,24 +24,44 @@
               <i class="fa fa-bell cursor-pointer"></i>
             </a>
             <ul class="dropdown-menu  dropdown-menu-end  px-2 py-3 me-sm-n4" aria-labelledby="dropdownMenuButton">
+              @php
+                $idUser = Auth::user()->id;
+                $array = App\Models\Approval::Select('name','approved','level','created_at','user_id','proposal_id','date')->where('approved',1)->where('user_id', $idUser)->orderBy('date', 'DESC')->paginate(5);
+              @endphp
+              @forelse($array as $element)
               <li class="mb-2">
                 <a class="dropdown-item border-radius-md" href="javascript:;">
                   <div class="d-flex py-1">
-                    <div class="my-auto">
-                      <img src="../assets/img/team-2.jpg" class="avatar avatar-sm  me-3 ">
-                    </div>
                     <div class="d-flex flex-column justify-content-center">
                       <h6 class="text-sm font-weight-normal mb-1">
-                        <span class="font-weight-bold">New message</span> from Laur
-                      </h6>
-                      <p class="text-xs text-secondary mb-0">
-                        <i class="fa fa-clock me-1"></i>
-                        13 minutes ago
+                        <span class="font-weight-bold">{{ $element->proposal->name }} </span>  
+                        </h6>
+                        <p>Sudah disetujui <span class="badge bg-success text-white">{{ $element->name }}</span></p>
+                        
+                      <p class="text-xs text-black mb-0">
+                        <i class="fa fa-clock me-1"> {{ $element->date }}</i>
+                        
                       </p>
                     </div>
                   </div>
                 </a>
+              </li> 
+              <hr>
+              @empty
+              <li class="mb-2">
+                <a class="dropdown-item border-radius-md" href="javascript:;">
+                  <div class="d-flex py-1">
+                    <div class="d-flex flex-column justify-content-center">
+                      <h6 class="text-sm font-weight-normal mb-1">
+                        <span class="font-weight-bold">Belum ada notifikasi</span>
+                      </h6>
+                    </div>
+                  </div>
+                </a>
               </li>
+             
+              @endforelse
+              
             </ul>
           </li>
           <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
