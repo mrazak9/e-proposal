b9c31adf5670d4826f4e332a2131d41c1e451122
codeguna <pratamagunadhi@gmail.com>
2022-09-05 09:15:47 +0700

    prevent error when doesnt have relation org id

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -16,9 +16,10 @@
                                 <h3>{{ __('Student') }}</h3>
                             </span>
 
-                             <div class="float-right">
-                                <button class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#studentModal"><i class="fa fa-plus"></i></button>
-                              </div>
+                            <div class="float-right">
+                                <button class="btn btn-sm btn-primary" data-bs-toggle="modal"
+                                    data-bs-target="#studentModal"><i class="fa fa-plus"></i></button>
+                            </div>
                         </div>
                     </div>
                     @if ($message = Session::get('success'))
@@ -26,20 +27,20 @@
                             <p>{{ $message }}</p>
                         </div>
                     @endif
-                        @include('student.modal')
+                    @include('student.modal')
                     <div class="card-body">
                         <div class="table-responsive">
                             <table class="table table-striped table-hover">
                                 <thead class="thead">
                                     <tr>
                                         <th>No</th>
-                                        
-										<th>Nama</th>
-										<th>Nim</th>
-										<th>Prodi</th>
-										<th>Kelas</th>
-										<th>Organisasi</th>
-										<th>Position</th>
+
+                                        <th>Nama</th>
+                                        <th>Nim</th>
+                                        <th>Prodi</th>
+                                        <th>Kelas</th>
+                                        <th>Organisasi</th>
+                                        <th>Position</th>
 
                                         <th></th>
                                     </tr>
@@ -48,21 +49,34 @@
                                     @foreach ($students as $student)
                                         <tr class="align-middle">
                                             <td>{{ ++$i }}</td>
-                                            
-											<td>{{ $student->user->name }}</td>
-											<td>{{ $student->nim }}</td>
-											<td>{{ $student->prodi }}</td>
-											<td>{{ $student->kelas }}</td>
-											<td>{{ $student->organization->name }}</td>
-											<td>{{ $student->position }}</td>
+
+                                            <td>{{ $student->user->name }}</td>
+                                            <td>{{ $student->nim }}</td>
+                                            <td>{{ $student->prodi }}</td>
+                                            <td>{{ $student->kelas }}</td>
+                                            <td>
+                                                @if ($student->organization()->exists())
+                                                    {{ $student->organization->name }}
+                                                @else
+                                                    <span class="badge bg-warning text-white">Belum ada organisasi</span>
+                                                @endif
+                                            </td>
+                                            <td>{{ $student->position }}</td>
 
                                             <td>
-                                                <form action="{{ route('admin.students.destroy',$student->id) }}" method="POST">
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('admin.students.show',$student->id) }}"><i class="bi bi-eye-fill"></i></a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('admin.students.edit',$student->id) }}"><i class="bi bi-pencil"></i></a>
+                                                <form action="{{ route('admin.students.destroy', $student->id) }}"
+                                                    method="POST">
+                                                    <a class="btn btn-sm btn-primary "
+                                                        href="{{ route('admin.students.show', $student->id) }}"><i
+                                                            class="bi bi-eye-fill"></i></a>
+                                                    <a class="btn btn-sm btn-success"
+                                                        href="{{ route('admin.students.edit', $student->id) }}"><i
+                                                            class="bi bi-pencil"></i></a>
                                                     @csrf
                                                     @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('{{ trans('global.areYouSure') }}');"><i class="bi bi-trash"></i></button>
+                                                    <button type="submit" class="btn btn-danger btn-sm"
+                                                        onclick="return confirm('{{ trans('global.areYouSure') }}');"><i
+                                                            class="bi bi-trash"></i></button>
                                                 </form>
                                             </td>
                                         </tr>
