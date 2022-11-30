ad9a408f234a9625e9c809cfeca3e78a4861f738
codeguna <pratamagunadhi@gmail.com>
2022-11-30 16:28:44 +0700

    limit view pencairan dana

--- resources/views/proposal/show.blade.php
+++ resources/views/proposal/show.blade.php
@@ -135,59 +135,61 @@
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <hr>
-                    </div>
-                    <div class="col-md-12">
-                        <div class="card h-100 mb-4">
-                            <div class="card-header pb-0 px-3">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <h6 class="mb-0">Pencairan Dana</h6>
-                                    </div>
-                                    <div
-                                        class="col-md-6 d-flex justify-content-start justify-content-md-end align-items-center">
-
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="card-body pt-4 p-3">
-                                <h6 class="text-uppercase text-body text-xs font-weight-bolder mb-3">Logs
-                                </h6>
-                                <ul class="list-group">
-                                    <li
-                                        class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
-                                        <div class="d-flex align-items-center">
-                                            <button
-                                                class="btn btn-icon-only btn-rounded btn-outline-success mb-0 me-3 p-3 btn-sm d-flex align-items-center justify-content-center">1
-                                            </button>
-                                            <div class="d-flex flex-column">
-                                                <h6 class="mb-1 text-dark text-sm">Netflix</h6>
-                                                <span class="text-xs">27 March 2020, at 12:30 PM</span>
-                                            </div>
+                @hasrole('bas')
+                    <div class="row">
+                        <div class="col-md-12">
+                            <hr>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="card h-100 mb-4">
+                                <div class="card-header pb-0 px-3">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <h6 class="mb-0">Pencairan Dana</h6>
                                         </div>
                                         <div
-                                            class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold">
-                                            + $ 2,500
+                                            class="col-md-6 d-flex justify-content-start justify-content-md-end align-items-center">
+
                                         </div>
-                                    </li>
-                                </ul>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <h6 class="mb-0">Dana telah dicairkan senilai:</h6>
-                                    </div>
-                                    <div
-                                        class="col-md-6 d-flex justify-content-start justify-content-md-end align-items-center">
-                                        <i class="fa fa-money p-3 text-success" aria-hidden="true"></i>
-                                        <strong class="text-success">Rp. 6.000.000 dari Rp. 7.000.000</strong>
                                     </div>
                                 </div>
+                                <div class="card-body pt-4 p-3">
+                                    <h6 class="text-uppercase text-body text-xs font-weight-bolder mb-3">Logs
+                                    </h6>
+                                    <ul class="list-group">
+                                        <li
+                                            class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+                                            <div class="d-flex align-items-center">
+                                                <button
+                                                    class="btn btn-icon-only btn-rounded btn-outline-success mb-0 me-3 p-3 btn-sm d-flex align-items-center justify-content-center">1
+                                                </button>
+                                                <div class="d-flex flex-column">
+                                                    <h6 class="mb-1 text-dark text-sm">Netflix</h6>
+                                                    <span class="text-xs">27 March 2020, at 12:30 PM</span>
+                                                </div>
+                                            </div>
+                                            <div
+                                                class="d-flex align-items-center text-success text-gradient text-sm font-weight-bold">
+                                                + $ 2,500
+                                            </div>
+                                        </li>
+                                    </ul>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <h6 class="mb-0">Dana telah dicairkan senilai:</h6>
+                                        </div>
+                                        <div
+                                            class="col-md-6 d-flex justify-content-start justify-content-md-end align-items-center">
+                                            <i class="fa fa-money p-3 text-success" aria-hidden="true"></i>
+                                            <strong class="text-success">Rp. 6.000.000 dari Rp. 7.000.000</strong>
+                                        </div>
+                                    </div>
 
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                @endhasrole
     </section>
     <br />
     <section class="content container-fluid">
