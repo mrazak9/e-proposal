d93d23c871a69022c1d817c61357b1c59d8c2207
codeguna <pratamagunadhi@gmail.com>
2023-01-02 15:13:41 +0700

    create report next page

--- resources/views/proposal/report/print.blade.php
+++ resources/views/proposal/report/print.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/font-awesome-animation.css') }}" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <title>Cetak Proposal | {{ ucwords($proposals->name) }}</title>
     <style>
@@ -33,7 +35,10 @@
             <div class="col-md-12">
                 <h1>Institut Digital Ekonomi</h1>
                 <h1>LPKIA</h1>
-                <h2>{{ $proposals->name }}</h2>
+                <br>
+                <br>
+                <h2>{{ ucwords($proposals->name) }}</h2>
+                <br>
                 <h3>disusun <br />oleh</h3>
                 <h3>{{ $proposals->org_name }}</h3>
             </div>
@@ -42,7 +47,7 @@
                         src="{{ asset('images/CAP LPKIA.png') }}" alt="LPKIA logo" height="100px"></p>
 
             </div>
-            <div class="col-md-12" style="margin-top: 50%">
+            <div class="col-md-12" style="margin-top: 40%">
                 <h3>Jalan Soekarno Hatta No. 456</h3>
                 <h3>Bandung 40266, Jawa Barat</h3>
                 <h3><i class="fas fa-phone"></i> 022-7564283 / 7564284</h3>
@@ -52,32 +57,7 @@
         {{-- ISI PROPOSAL --}}
         <div class="row">
             <div class="col-md-12">
-                <div class="table-responsive">
-                    <div class="table-responsive">
-                        <table class="table table-primary">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Column 1</th>
-                                    <th scope="col">Column 2</th>
-                                    <th scope="col">Column 3</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr class="">
-                                    <td scope="row">R1C1</td>
-                                    <td>R1C2</td>
-                                    <td>R1C3</td>
-                                </tr>
-                                <tr class="">
-                                    <td scope="row">Item</td>
-                                    <td>Item</td>
-                                    <td>Item</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-
-                </div>
+                <p>Nama Kegiatan: {{ $proposals->name }}</p>
             </div>
         </div>
         {{-- END OF ISI PROPOSAL --}}
