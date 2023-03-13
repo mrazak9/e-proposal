5ee2122e7dc6ca1e51129a59ed532fa9100fdb8b
codeguna <pratamagunadhi@gmail.com>
2023-03-13 10:29:46 +0700

    set view DOP

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -126,18 +126,19 @@
                         (DOP) <i class="bi bi-caret-right-fill"></i></h6>
                 </li>
             @endcan
-            <div id="dop">
-                <li class="nav-item">
-                    <a class="nav-link text-white {{ request()->is('admin/student/member') || request()->is('admin/student/member/*') ? 'active bg-gradient-primary' : '' }}"
-                        href="{{ route('admin.dops.index') }}">
-                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                            <i class="bi bi-pencil-square"></i>
-                        </div>
-                        <span class="nav-link-text ms-1">Pengajuan DOP</span>
-                    </a>
-                </li>
-            </div>
-
+            @can('VIEW_DOP')
+                <div id="dop">
+                    <li class="nav-item">
+                        <a class="nav-link text-white {{ request()->is('admin/student/member') || request()->is('admin/student/member/*') ? 'active bg-gradient-primary' : '' }}"
+                            href="{{ route('admin.dops.index') }}">
+                            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                                <i class="bi bi-pencil-square"></i>
+                            </div>
+                            <span class="nav-link-text ms-1">Pengajuan DOP</span>
+                        </a>
+                    </li>
+                </div>
+            @endcan
             @hasanyrole('ADMIN|KETUA_HIMATIK|KETUA_HIMAADBIS|KETUA_HIMAKOMPAK|KETUA_UKM|KETUA_KSM|KETUA_BEM|KETUA_BPM')
                 <li class="nav-item mt-3" data-bs-toggle="collapse" data-bs-target="#organisasi">
                     <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Organisasi <i
