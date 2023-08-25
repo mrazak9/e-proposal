66e2b9777c378ea3e856a6ddf94d23b247c5429d
codeguna <pratamagunadhi@gmail.com>
2023-08-25 12:58:36 +0700

    fix dop

--- app/Models/Dop.php
+++ app/Models/Dop.php
@@ -55,7 +55,7 @@ class Dop extends Model
   }
   public function receiptfundsdop()
   {
-    return $this->hasMany('App\Models\ReceiptOfFundsDop');
+    return $this->hasOne('App\Models\ReceiptOfFundsDop');
   }
   public function dopRevision()
   {
