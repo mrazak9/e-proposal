9b6a4a75cb47c7aa513056e0d66729a9d945429a
codeguna <pratamagunadhi@gmail.com>
2023-08-26 10:08:04 +0700

    adjustment dop

--- app/Exports/RekapExporter.php
+++ app/Exports/RekapExporter.php
@@ -4,6 +4,7 @@ namespace App\Exports;
 
 use App\Models\Dop;
 use App\Models\Proposal;
+use App\Models\ReceiptOfFundsDop;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
 
@@ -29,10 +30,11 @@ class RekapExporter implements FromView
             [$this->startDate, $this->endDate])
             ->orderBy('created_at', 'ASC')->get(),
             
-            'dops' => Dop::where('isApproved', 1)
-            ->whereBetween('created_at', 
+            'dops' => ReceiptOfFundsDop::whereHas('dop', function ($query) {
+                $query->where('isApproved', 1);
+            })->whereBetween('created_at', 
             [$this->startDate, $this->endDate])
-            ->orderBy('created_at', 'ASC')->get()        
+            ->orderBy('created_at', 'ASC')->get()      
         ]);
     }
 }
