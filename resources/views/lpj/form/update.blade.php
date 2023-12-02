73ae43e01ffedae408bef99c1e51a3e866360172
codeguna <pratamagunadhi@gmail.com>
2023-12-02 09:25:28 +0700

    edit permissions

--- resources/views/lpj/form/update.blade.php
+++ resources/views/lpj/form/update.blade.php
@@ -3,19 +3,28 @@
     <div class="col-md-6">
         <div class="mb-3">
             <label class="form-label">Keberhasilan</label>
-            <textarea class="form-control" name="keberhasilan" rows="5" id="tinymce">{{ $lpj->keberhasilan }}</textarea>
+            <textarea class="form-control" name="keberhasilan" rows="5"
+                @can('PANITIA_UPDATE_PROPOSAL')
+                id="tinymce"
+            @endcan>{{ $lpj->keberhasilan }}</textarea>
         </div>
     </div>
     <div class="col-md-6">
         <div class="mb-3">
             <label class="form-label">Kendala</label>
-            <textarea class="form-control" name="kendala" rows="5" id="tinymce">{{ $lpj->kendala }}</textarea>
+            <textarea class="form-control" name="kendala" rows="5"
+                @can('PANITIA_UPDATE_PROPOSAL')
+            id="tinymce"
+        @endcan>{{ $lpj->kendala }}</textarea>
         </div>
     </div>
     <div class="col-md-12">
         <div class="mb-3">
             <label class="form-label">Notes</label>
-            <textarea class="form-control" name="notes" rows="5" id="tinymce">{{ $lpj->notes }}</textarea>
+            <textarea class="form-control" name="notes" rows="5"
+                @can('PANITIA_UPDATE_PROPOSAL')
+            id="tinymce"
+        @endcan>{{ $lpj->notes }}</textarea>
         </div>
     </div>
     <div class="col-md-6">
@@ -62,8 +71,6 @@
                     <i class="fas fa-download"></i> Unduh LPJ Proposal
                 </a>
             @endif
-
-
         </div>
     </div>
 </div>
